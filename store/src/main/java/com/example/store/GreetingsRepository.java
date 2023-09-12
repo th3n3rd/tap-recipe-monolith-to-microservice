066921f370cbb4c1585aa688d9345764b4e3262c
Marco Garofalo <mgarofalo@vmware.com>
2023-09-12 15:55:14 +0100

    Remove walking skeleton files and data

--- store/src/main/java/com/example/store/GreetingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.store;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GreetingsRepository extends JpaRepository<Greeting, Long> {}
