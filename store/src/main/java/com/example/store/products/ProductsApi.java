605f447308cfebecea562938a1038fa96d6711ac
Marco Garofalo <mgarofalo@vmware.com>
2023-09-13 13:11:21 +0100

    Move the product edition parameter in the query string

--- store/src/main/java/com/example/store/products/ProductsApi.java
+++ store/src/main/java/com/example/store/products/ProductsApi.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,8 +13,8 @@ class ProductsApi {
 
     private final ProductsCatalogue productsCatalogue;
 
-    @GetMapping("/products/{productId}/{edition}")
-    String productDetails(@PathVariable String productId, @PathVariable String edition, Model model) {
+    @GetMapping("/products/{productId}")
+    String productDetails(@PathVariable String productId, @RequestParam(defaultValue = "standard") String edition, Model model) {
         model.addAttribute("product", productsCatalogue.findById(productId).orElseThrow());
         model.addAttribute("edition", edition);
         return "product";
