19b0df2a5c3207295218b846f732e8e734e8bcca
Marco Garofalo <mgarofalo@vmware.com>
2023-09-15 21:01:59 +0100

    Make the recommendations api endpoint cleaner

--- store/src/main/java/com/example/store/recommendations/RecommendationsApi.java
+++ store/src/main/java/com/example/store/recommendations/RecommendationsApi.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,8 +13,8 @@ class RecommendationsApi {
 
     private final ReadOnlyProductsCatalogue productsCatalogue;
 
-    @GetMapping(value = "/recommendations/{productId}", produces = "text/fragment+html")
-    String listProducts(@PathVariable String productId, Model model) {
+    @GetMapping(value = "/recommendations", produces = "text/fragment+html")
+    String listProducts(@RequestParam String productId, Model model) {
         model.addAttribute("products", productsCatalogue.findAllByIdIsNot(productId));
         return "recommendations";
     }
