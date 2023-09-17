c69ad4de4229668e79c6251742b58c1ab0903ac7
Marco Garofalo <mgarofalo@vmware.com>
2023-09-17 18:12:49 +0100

    Move the table name to the @Table annotation to stay consistent

--- store/src/main/java/com/example/store/checkout/Pricing.java
+++ store/src/main/java/com/example/store/checkout/Pricing.java
@@ -2,11 +2,13 @@ package com.example.store.checkout;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
-@Entity(name = "pricing")
+@Entity
+@Table(name = "pricing")
 class Pricing {
     @Id
     private Long id;
