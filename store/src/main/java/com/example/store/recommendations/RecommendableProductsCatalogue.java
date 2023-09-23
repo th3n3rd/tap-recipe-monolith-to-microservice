7510c3c4e2eaf77f7fe9247fc3e47a9f06519c07
Marco Garofalo <mgarofalo@vmware.com>
2023-09-23 11:10:11 +0100

    Align naming convention for the products repositories

--- store/src/main/java/com/example/store/recommendations/RecommendableProductsCatalogue.java
+++ store/src/main/java/com/example/store/recommendations/RecommendableProductsCatalogue.java
@@ -4,6 +4,6 @@ package com.example.store.recommendations;
 import java.util.List;
 import org.springframework.data.repository.Repository;
 
-interface ReadOnlyProductsCatalogue extends Repository<RecommendableProduct, String> {
+interface RecommendableProductsCatalogue extends Repository<RecommendableProduct, String> {
     List<RecommendableProduct> findAllByIdIsNot(String productId);
 }
--- store/src/main/java/com/example/store/recommendations/RecommendationsApi.java
+++ store/src/main/java/com/example/store/recommendations/RecommendationsApi.java
@@ -4,14 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
 class RecommendationsApi {
 
-    private final ReadOnlyProductsCatalogue productsCatalogue;
+    private final RecommendableProductsCatalogue productsCatalogue;
 
     @GetMapping(value = "/recommendations", produces = "text/fragment+html")
     String listProducts(@RequestParam String productId, Model model) {
