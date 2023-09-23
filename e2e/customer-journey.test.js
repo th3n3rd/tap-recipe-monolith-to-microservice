5fc10666e4bf7d7dad52066392032d557580e82e
Marco Garofalo <mgarofalo@vmware.com>
2023-09-23 17:33:23 +0100

    Move front office dsl operations into a class to be reusable

--- e2e/customer-journey.test.js
+++ e2e/customer-journey.test.js
@@ -1,138 +1,114 @@
-import {
-    buyDisplayedProduct,
-    checkout,
-    continueShopping,
-    displaysOrderConfirmation,
-    displaysPageTitle,
-    displaysPlatinumProductImage,
-    displaysProductPrice,
-    displaysProductTitle,
-    displaysRecommendations,
-    displaysShoppingCartTitle,
-    displaysStandardProductImage,
-    emptyShoppingCart,
-    emptyShoppingCartIfNotEmpty,
-    orderContainsItems,
-    orderTotals,
-    reviewShoppingCart,
-    selectRecommendedProduct,
-    shoppingCartContains,
-    shoppingCartContainsItems,
-    shoppingCartIsEmpty,
-    shoppingCartTotals,
-    switchToPlatinumEdition
-} from "./storefront-dsl.js";
+import { Storefront } from "./storefront-dsl.js";
 import { openWebBrowser, reloadPage, visitPage } from "./test-utils.js";
 import { afterAll, beforeAll, beforeEach, describe, expect, test } from "vitest";
 
 describe("Customer Journey", () => {
     let browser;
-    let tab;
-    let document;
-    let url;
+    let baseUrl;
     let debugEnabled;
+    let storefront;
 
     beforeAll(async () => {
-        url = process.env.APP_URL || "http://localhost:8080";
+        baseUrl = process.env.APP_URL || "http://localhost:8080";
         debugEnabled = !!process.env.DEBUG_MODE;
         browser = await openWebBrowser(debugEnabled);
-        tab = await browser.newPage();
+        storefront = new Storefront(await browser.newPage(), baseUrl);
     });
 
     beforeEach(async () => {
-        document = await visitPage(tab, url);
-    })
+        await storefront.visitHomePage();
+    });
 
     afterAll(async () => {
         await browser.close();
     });
 
     test("Displaying products and browsing recommendations", async () => {
-        await displaysPageTitle(document, /The Tractor Store/);
-        await displaysProductTitle(document, "Eicher Diesel 215/16");
-        await displaysStandardProductImage(document, "Eicher Diesel 215/16");
-        await displaysProductPrice(document, "$58");
-        await displaysRecommendations(document, [
+        await storefront.displaysPageTitle(/The Tractor Store/);
+        await storefront.displaysProductTitle("Eicher Diesel 215/16");
+        await storefront.displaysStandardProductImage("Eicher Diesel 215/16");
+        await storefront.displaysProductPrice("$58");
+        await storefront.displaysRecommendations([
             "Fendt F20 Dieselroß",
             "Porsche-Diesel Master 419"
         ]);
 
-        await switchToPlatinumEdition(document);
-        await displaysPlatinumProductImage(document, "Eicher Diesel 215/16");
+        await storefront.switchToPlatinumEdition();
+        await storefront.displaysPlatinumProductImage("Eicher Diesel 215/16");
 
-        await selectRecommendedProduct(document, "Porsche-Diesel Master 419")
-        await displaysProductTitle(document, "Porsche-Diesel Master 419");
-        await displaysStandardProductImage(document, "Porsche-Diesel Master 419");
-        await displaysProductPrice(document, "$66");
-        await displaysRecommendations(document, [
+        await storefront.selectRecommendedProduct("Porsche-Diesel Master 419")
+        await storefront.displaysProductTitle("Porsche-Diesel Master 419");
+        await storefront.displaysStandardProductImage("Porsche-Diesel Master 419");
+        await storefront.displaysProductPrice("$66");
+        await storefront.displaysRecommendations([
             "Fendt F20 Dieselroß",
             "Eicher Diesel 215/16"
         ]);
 
-        document = await reloadPage(tab);
-        await displaysPageTitle(document, /The Tractor Store/);
-        await displaysProductTitle(document, "Porsche-Diesel Master 419");
+        await storefront.reloadPage();
+        await storefront.displaysPageTitle(/The Tractor Store/);
+        await storefront.displaysProductTitle("Porsche-Diesel Master 419");
     });
 
     test("Track products in the shopping cart", async () => {
-        await emptyShoppingCartIfNotEmpty(document);
+        await storefront.emptyShoppingCartIfNotEmpty();
 
-        await buyDisplayedProduct(document);
-        await shoppingCartContains(document, 1);
-        await shoppingCartTotals(document, "$58");
-        await reviewShoppingCart(document);
-        await displaysShoppingCartTitle(document);
-        await shoppingCartContainsItems(document, [
+        await storefront.buyDisplayedProduct();
+        await storefront.shoppingCartContains(1);
+        await storefront.shoppingCartTotals("$58");
+        await storefront.reviewShoppingCart();
+        await storefront.displaysShoppingCartTitle();
+        await storefront.shoppingCartContainsItems([
             { model: "Eicher Diesel 215/16", edition: "standard", price: "$58" },
         ]);
-        await shoppingCartTotals(document, "$58");
-        await continueShopping(document);
-        await emptyShoppingCart(document);
-        await shoppingCartIsEmpty(document);
+        await storefront.shoppingCartTotals("$58");
+        await storefront.continueShopping();
+        await storefront.emptyShoppingCart();
+        await storefront.shoppingCartIsEmpty();
 
-        await buyDisplayedProduct(document);
-        await selectRecommendedProduct(document, "Porsche-Diesel Master 419")
-        await switchToPlatinumEdition(document);
-        await buyDisplayedProduct(document);
-        await shoppingCartContains(document, 2);
-        await shoppingCartTotals(document, "$1024");
+        await storefront.buyDisplayedProduct();
+        await storefront.selectRecommendedProduct("Porsche-Diesel Master 419")
+        await storefront.switchToPlatinumEdition();
+        await storefront.buyDisplayedProduct();
+        await storefront.shoppingCartContains(2);
+        await storefront.shoppingCartTotals("$1024");
 
-        await reviewShoppingCart(document);
-        await shoppingCartContainsItems(document, [
+        await storefront.reviewShoppingCart();
+        await storefront.shoppingCartContainsItems([
             { model: "Eicher Diesel 215/16", edition: "standard", price: "$58" },
             { model: "Porsche-Diesel Master 419", edition: "platinum", price: "$966" },
         ]);
-        await shoppingCartTotals(document, "$1024");
+        await storefront.shoppingCartTotals("$1024");
     });
 
     test("Checkout products in the shopping cart", async () => {
-        await emptyShoppingCartIfNotEmpty(document);
+        await storefront.emptyShoppingCartIfNotEmpty();
 
-        await buyDisplayedProduct(document);
-        await reviewShoppingCart(document);
-        await displaysShoppingCartTitle(document);
-        await checkout(document);
-        const firstOrderNumber = await displaysOrderConfirmation(document);
-        await orderContainsItems(document, [
+        await storefront.buyDisplayedProduct();
+        await storefront.reviewShoppingCart();
+        await storefront.displaysShoppingCartTitle();
+        await storefront.checkout();
+        const firstOrderNumber = await storefront.displaysOrderConfirmation();
+        await storefront.orderContainsItems([
             { model: "Eicher Diesel 215/16", edition: "standard", price: "$58" }
         ]);
-        await orderTotals(document, "$58")
-        await continueShopping(document);
-        await shoppingCartIsEmpty(document);
+        await storefront.orderTotals("$58")
+        await storefront.continueShopping();
+        await storefront.shoppingCartIsEmpty();
 
-        await selectRecommendedProduct(document, "Porsche-Diesel Master 419")
-        await buyDisplayedProduct(document);
-        await selectRecommendedProduct(document, "Fendt F20 Dieselroß")
-        await switchToPlatinumEdition(document);
-        await buyDisplayedProduct(document);
-        await checkout(document);
-        const secondOrderNumber = await displaysOrderConfirmation(document);
+        await storefront.selectRecommendedProduct("Porsche-Diesel Master 419")
+        await storefront.buyDisplayedProduct();
+        await storefront.selectRecommendedProduct("Fendt F20 Dieselroß")
+        await storefront.switchToPlatinumEdition();
+        await storefront.buyDisplayedProduct();
+        await storefront.checkout();
+        const secondOrderNumber = await storefront.displaysOrderConfirmation();
         expect(secondOrderNumber).not.toEqual(firstOrderNumber);
-        await orderContainsItems(document, [
+        await storefront.orderContainsItems([
             { model: "Fendt F20 Dieselroß", edition: "platinum", price: "$954" },
             { model: "Porsche-Diesel Master 419", edition: "standard", price: "$66" },
         ]);
-        await orderTotals(document, "$1020")
+        await storefront.orderTotals("$1020")
     });
 
 }, { timeout: 120_000 });
--- e2e/storefront-dsl.js
+++ e2e/storefront-dsl.js
@@ -1,246 +1,266 @@
-import { queries } from "pptr-testing-library";
+import { getDocument, queries } from "pptr-testing-library";
 import { expect } from "vitest";
 
 const timeout = 5000;
 const waitForOptions = { timeout: timeout }
 
-function waitForHtmxToSettle() {
-    // htmx by default uses 20ms in order to settle the new attributes on inserted elements
-    // this seems to affect buttons/clicks that might happen right after insertion
-    return new Promise(resolve => setTimeout(resolve, 50));
-}
+export class Storefront {
+    tab;
+    document;
+    baseUrl;
 
-async function htmxSafeClick(element) {
-    await waitForHtmxToSettle();
-    await element.click();
-}
-
-export async function displaysPageTitle(document, title) {
-    try {
-        await queries.findByRole(document, "heading", { level: 1, name: title }, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, displaysPageTitle);
-        throw error;
+    constructor(tab, baseUrl) {
+        this.baseUrl = baseUrl;
+        this.tab = tab;
     }
-}
 
-export async function displaysProductTitle(document, model) {
-    try {
-        await queries.findByRole(document, "heading", { level: 2, name: model }, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, displaysProductTitle);
-        throw error;
+    async visitHomePage() {
+        await this.tab.goto(`${this.baseUrl}/`);
+        this.document = await getDocument(this.tab);
     }
-}
-
-export async function switchToPlatinumEdition(document) {
-    try {
-        const checkbox = await queries.findByLabelText(document, /Platinum Edition/, {}, waitForOptions);
-        await htmxSafeClick(checkbox);
-    } catch (error) {
-        Error.captureStackTrace(error, switchToPlatinumEdition);
-        throw error;
+    
+    async reloadPage() {
+        await this.tab.reload();
+        this.document = await getDocument(this.tab);
     }
-}
 
-export async function displaysStandardProductImage(document, product) {
-    try {
-        await queries.findByAltText(document, new RegExp(`^The standard edition version of the ${product}`), {}, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, displaysStandardProductImage);
-        throw error;
-    }
-}
-
-export async function displaysPlatinumProductImage(document, product) {
-    try {
-        await queries.findByAltText(document, new RegExp(`^The platinum edition version of the ${product}`), {}, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, displaysPlatinumProductImage);
-        throw error;
-    }
-}
-
-export async function displaysProductPrice(document, price) {
-    try {
-        await queries.findByText(document, new RegExp(`Buy for \\${price}`), {}, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, buyDisplayedProduct);
-        throw error;
-    }
-}
-
-export async function buyDisplayedProduct(document) {
-    try {
-        const buy = await queries.findByRole(document, "button", { name: /Buy for/ }, waitForOptions);
-        await htmxSafeClick(buy);
-    } catch (error) {
-        Error.captureStackTrace(error, buyDisplayedProduct);
-        throw error;
-    }
-}
-
-export async function shoppingCartContains(document, numberOfElements) {
-    try {
-        await queries.findByText(document, new RegExp(`You've picked ${numberOfElements} items`), {}, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, shoppingCartContains);
-        throw error;
-    }
-}
-
-export async function shoppingCartIsEmpty(document) {
-    try {
-        await queries.findByText(document, new RegExp(`Your cart is empty`), {}, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, shoppingCartIsEmpty);
-        throw error;
-    }
-}
-
-export async function reviewShoppingCart(document){
-    try {
-        const review = await queries.findByRole(document, "button", { name: /View cart/ }, waitForOptions);
-        await htmxSafeClick(review);
-    } catch (error) {
-        Error.captureStackTrace(error, reviewShoppingCart);
-        throw error;
-    }
-}
-
-export async function displaysShoppingCartTitle(document) {
-    try {
-        await queries.findByRole(document, "heading", { level: 2, name: /Shopping Cart/ }, waitForOptions)
-    } catch (error) {
-        Error.captureStackTrace(error, displaysShoppingCartTitle);
-        throw error;
-    }
-}
-
-export async function shoppingCartContainsItems(document, expectedItems) {
-    try {
-        const rows = await queries.findAllByRole(document, "row", {}, waitForOptions);
-        const items = rows.slice(1, rows.length - 1); // remove the headers and the total amount
-        expect(items.length).toEqual(expectedItems.length);
-        for (let i = 0; i < items.length; i++){
-            const item = items[i];
-            const expectedItem = expectedItems[i];
-            const content = await item.evaluate(element => element.textContent)
-            expect(content).toContain(expectedItem.model);
-            expect(content).toContain(expectedItem.edition);
-            expect(content).toContain(expectedItem.price);
+    async displaysPageTitle(title) {
+        try {
+            await queries.findByRole(this.document, "heading", { level: 1, name: title }, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysPageTitle);
+            throw error;
         }
-    } catch (error) {
-        Error.captureStackTrace(error, shoppingCartContainsItems);
-        throw error;
     }
-}
 
-export async function shoppingCartTotals(document, total) {
-    try {
-        await queries.findByText(document, new RegExp(`for a total of \\${total}`), {}, waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, shoppingCartTotals);
-        throw error;
-    }
-}
-
-export async function emptyShoppingCart(document) {
-    try {
-        const emptyCart = await queries.findByRole(document, "button", { name: /Empty cart/ }, waitForOptions);
-        await htmxSafeClick(emptyCart);
-    } catch (error) {
-        Error.captureStackTrace(error, emptyShoppingCart);
-        throw error;
-    }
-}
-
-export async function emptyShoppingCartIfNotEmpty(document) {
-    try {
-        const emptyCart = await queries.findByRole(document, "button", { name: /Empty cart/ }, waitForOptions);
-        await htmxSafeClick(emptyCart);
-    } catch (error) {
-        // ignore errors
-    }
-}
-
-export async function displaysRecommendations(document, products) {
-    try {
-        const recommendations = await queries.findByRole(document, "region", { name: /Recommendations/ }, waitForOptions);
-        for (const product of products) {
-            await queries.findByAltText(recommendations, new RegExp(`^A recommendation for the standard edition version of the ${product}`), {}, waitForOptions);
+    async displaysProductTitle(model) {
+        try {
+            await queries.findByRole(this.document, "heading", { level: 2, name: model }, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysProductTitle);
+            throw error;
         }
-    } catch (error) {
-        Error.captureStackTrace(error, displaysRecommendations);
-        throw error;
     }
-}
 
-export async function selectRecommendedProduct(document, product) {
-    try {
-        const recommendation = await queries.findByAltText(document, new RegExp(`^A recommendation for the standard edition version of the ${product}`), {}, waitForOptions);
-        await htmxSafeClick(recommendation);
-    } catch (error) {
-        Error.captureStackTrace(error, selectRecommendedProduct);
-        throw error;
-    }
-}
-
-export async function checkout(document) {
-    try {
-        const checkout = await queries.findByRole(document, "button", { name: /Checkout/ }, waitForOptions);
-        await htmxSafeClick(checkout);
-    } catch (error) {
-        Error.captureStackTrace(error, checkout);
-        throw error;
-    }
-}
-
-export async function displaysOrderConfirmation(document) {
-    try {
-        await queries.findByText(document, /Your order is confirmed!/, {}, waitForOptions);
-        const orderNumber = await queries.findByTestId(document, "order-number", {}, waitForOptions);
-        return await orderNumber.evaluate(element => element.textContent);
-    } catch (error) {
-        Error.captureStackTrace(error, displaysOrderConfirmation);
-        throw error;
-    }
-}
-
-export async function orderContainsItems(document, expectedItems) {
-    try {
-        const rows = await queries.findAllByRole(document, "row", {}, waitForOptions);
-        const items = rows.slice(1, rows.length - 1); // remove the headers and the total amount
-        expect(items.length).toEqual(expectedItems.length);
-        for (let i = 0; i < items.length; i++){
-            const item = items[i];
-            const expectedItem = expectedItems[i];
-            const content = await item.evaluate(element => element.textContent)
-            expect(content).toContain(expectedItem.model);
-            expect(content).toContain(expectedItem.edition);
-            expect(content).toContain(expectedItem.price);
+    async switchToPlatinumEdition() {
+        try {
+            const checkbox = await queries.findByLabelText(this.document, /Platinum Edition/, {}, waitForOptions);
+            await this.htmxSafeClick(checkbox);
+        } catch (error) {
+            Error.captureStackTrace(error, this.switchToPlatinumEdition);
+            throw error;
         }
-    } catch (error) {
-        Error.captureStackTrace(error, orderContainsItems);
-        throw error;
     }
-}
 
-export async function orderTotals(document, total) {
-    try {
-        await queries.findByText(document, new RegExp(`Total amount \\${total}`), waitForOptions);
-    } catch (error) {
-        Error.captureStackTrace(error, orderTotals);
-        throw error;
+    async displaysStandardProductImage(product) {
+        try {
+            await queries.findByAltText(this.document, new RegExp(`^The standard edition version of the ${product}`), {}, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysStandardProductImage);
+            throw error;
+        }
     }
-}
 
-export async function continueShopping(document) {
-    try {
-        const continueShopping = await queries.findByRole(document, "button", { name: /Continue shopping/ }, waitForOptions);
-        await htmxSafeClick(continueShopping);
-    } catch (error) {
-        Error.captureStackTrace(error, continueShopping);
-        throw error;
+    async displaysPlatinumProductImage(product) {
+        try {
+            await queries.findByAltText(this.document, new RegExp(`^The platinum edition version of the ${product}`), {}, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysPlatinumProductImage);
+            throw error;
+        }
+    }
+
+    async displaysProductPrice(price) {
+        try {
+            await queries.findByText(this.document, new RegExp(`Buy for \\${price}`), {}, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.buyDisplayedProduct);
+            throw error;
+        }
+    }
+
+    async buyDisplayedProduct() {
+        try {
+            const buy = await queries.findByRole(this.document, "button", { name: /Buy for/ }, waitForOptions);
+            await this.htmxSafeClick(buy);
+        } catch (error) {
+            Error.captureStackTrace(error, this.buyDisplayedProduct);
+            throw error;
+        }
+    }
+
+    async shoppingCartContains(numberOfElements) {
+        try {
+            await queries.findByText(this.document, new RegExp(`You've picked ${numberOfElements} items`), {}, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.shoppingCartContains);
+            throw error;
+        }
+    }
+
+    async shoppingCartIsEmpty() {
+        try {
+            await queries.findByText(this.document, new RegExp(`Your cart is empty`), {}, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.shoppingCartIsEmpty);
+            throw error;
+        }
+    }
+
+    async reviewShoppingCart(){
+        try {
+            const review = await queries.findByRole(this.document, "button", { name: /View cart/ }, waitForOptions);
+            await this.htmxSafeClick(review);
+        } catch (error) {
+            Error.captureStackTrace(error, this.reviewShoppingCart);
+            throw error;
+        }
+    }
+
+    async displaysShoppingCartTitle() {
+        try {
+            await queries.findByRole(this.document, "heading", { level: 2, name: /Shopping Cart/ }, waitForOptions)
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysShoppingCartTitle);
+            throw error;
+        }
+    }
+
+    async shoppingCartContainsItems(expectedItems) {
+        try {
+            const rows = await queries.findAllByRole(this.document, "row", {}, waitForOptions);
+            const items = rows.slice(1, rows.length - 1); // remove the headers and the total amount
+            expect(items.length).toEqual(expectedItems.length);
+            for (let i = 0; i < items.length; i++){
+                const item = items[i];
+                const expectedItem = expectedItems[i];
+                const content = await item.evaluate(element => element.textContent)
+                expect(content).toContain(expectedItem.model);
+                expect(content).toContain(expectedItem.edition);
+                expect(content).toContain(expectedItem.price);
+            }
+        } catch (error) {
+            Error.captureStackTrace(error, this.shoppingCartContainsItems);
+            throw error;
+        }
+    }
+
+    async shoppingCartTotals(total) {
+        try {
+            await queries.findByText(this.document, new RegExp(`for a total of \\${total}`), {}, waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.shoppingCartTotals);
+            throw error;
+        }
+    }
+
+    async emptyShoppingCart() {
+        try {
+            const emptyCart = await queries.findByRole(this.document, "button", { name: /Empty cart/ }, waitForOptions);
+            await this.htmxSafeClick(emptyCart);
+        } catch (error) {
+            Error.captureStackTrace(error, this.emptyShoppingCart);
+            throw error;
+        }
+    }
+
+    async emptyShoppingCartIfNotEmpty() {
+        try {
+            const emptyCart = await queries.findByRole(this.document, "button", { name: /Empty cart/ }, waitForOptions);
+            await this.htmxSafeClick(emptyCart);
+        } catch (error) {
+            // ignore errors
+        }
+    }
+
+    async displaysRecommendations(products) {
+        try {
+            const recommendations = await queries.findByRole(this.document, "region", { name: /Recommendations/ }, waitForOptions);
+            for (const product of products) {
+                await queries.findByAltText(recommendations, new RegExp(`^A recommendation for the standard edition version of the ${product}`), {}, waitForOptions);
+            }
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysRecommendations);
+            throw error;
+        }
+    }
+
+    async selectRecommendedProduct(product) {
+        try {
+            const recommendation = await queries.findByAltText(this.document, new RegExp(`^A recommendation for the standard edition version of the ${product}`), {}, waitForOptions);
+            await this.htmxSafeClick(recommendation);
+        } catch (error) {
+            Error.captureStackTrace(error, this.selectRecommendedProduct);
+            throw error;
+        }
+    }
+
+    async checkout() {
+        try {
+            const checkout = await queries.findByRole(this.document, "button", { name: /Checkout/ }, waitForOptions);
+            await this.htmxSafeClick(checkout);
+        } catch (error) {
+            Error.captureStackTrace(error, this.checkout);
+            throw error;
+        }
+    }
+
+    async displaysOrderConfirmation() {
+        try {
+            await queries.findByText(this.document, /Your order is confirmed!/, {}, waitForOptions);
+            const orderNumber = await queries.findByTestId(this.document, "order-number", {}, waitForOptions);
+            return await orderNumber.evaluate(element => element.textContent);
+        } catch (error) {
+            Error.captureStackTrace(error, this.displaysOrderConfirmation);
+            throw error;
+        }
+    }
+
+    async orderContainsItems(expectedItems) {
+        try {
+            const rows = await queries.findAllByRole(this.document, "row", {}, waitForOptions);
+            const items = rows.slice(1, rows.length - 1); // remove the headers and the total amount
+            expect(items.length).toEqual(expectedItems.length);
+            for (let i = 0; i < items.length; i++){
+                const item = items[i];
+                const expectedItem = expectedItems[i];
+                const content = await item.evaluate(element => element.textContent)
+                expect(content).toContain(expectedItem.model);
+                expect(content).toContain(expectedItem.edition);
+                expect(content).toContain(expectedItem.price);
+            }
+        } catch (error) {
+            Error.captureStackTrace(error, this.orderContainsItems);
+            throw error;
+        }
+    }
+
+    async orderTotals(total) {
+        try {
+            await queries.findByText(this.document, new RegExp(`Total amount \\${total}`), waitForOptions);
+        } catch (error) {
+            Error.captureStackTrace(error, this.orderTotals);
+            throw error;
+        }
+    }
+    async continueShopping() {
+        try {
+            const continueShopping = await queries.findByRole(this.document, "button", { name: /Continue shopping/ }, waitForOptions);
+            await this.htmxSafeClick(continueShopping);
+        } catch (error) {
+            Error.captureStackTrace(error, this.continueShopping);
+            throw error;
+        }
+    }
+
+    waitForHtmxToSettle() {
+        // htmx by default uses 20ms in order to settle the new attributes on inserted elements
+        // this seems to affect buttons/clicks that might happen right after insertion
+        return new Promise(resolve => setTimeout(resolve, 50));
+    }
+
+    async htmxSafeClick(element) {
+        await this.waitForHtmxToSettle();
+        await element.click();
     }
 }
