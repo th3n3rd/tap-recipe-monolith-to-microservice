184edf426e7ef710ce5886ad000597cb1f22f637
Marco Garofalo <mgarofalo@vmware.com>
2023-09-24 14:17:29 +0100

    Make it easier to create new database migrations

--- bin/new-db-migration.sh
+++ bin/new-db-migration.sh
@@ -4,7 +4,8 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
-MIGRATION_FILE="$SCRIPT_DIR/../store/src/main/resources/db/migration/V$(date +"%Y%m%d%H%M%S")__your-description-here.sql"
+DESCRIPTION=${1:-"your-description-here"}
+MIGRATION_FILE="$SCRIPT_DIR/../store/src/main/resources/db/migration/V$(date +"%Y%m%d%H%M%S")__$DESCRIPTION.sql"
 
 touch "$MIGRATION_FILE"
 
